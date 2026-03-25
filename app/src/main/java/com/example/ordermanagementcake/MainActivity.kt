97c62f158cf25c594f61b42442208c9b2a04b19e
abindacarmo <abindacarmo08@gmail.com>
2026-03-25 16:19:47 +0900

    fix(mainActivity): troka android ba ediko

--- app/src/main/java/com/example/ordermanagementcake/MainActivity.kt
+++ app/src/main/java/com/example/ordermanagementcake/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     OrderManagementCakeTheme {
-        Greeting("Android")
+        Greeting("Ediko")
     }
 }
