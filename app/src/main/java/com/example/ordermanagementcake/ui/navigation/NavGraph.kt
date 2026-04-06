c8636aba9ab446f1c9a61d0f7c8e4c0375066972
Noro18 <zeroxg534@gmail.com>
2026-04-06 11:27:43 +0900

    feature(NavGraph): Kria AppNavHost no define navigation nia routers sira

---
+++ app/src/main/java/com/example/ordermanagementcake/ui/navigation/NavGraph.kt
@@ -0,0 +1,27 @@
+package com.example.ordermanagementcake.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.ordermanagementcake.ui.clients.ClientsListScreen
+import com.example.ordermanagementcake.ui.orders.OrderListScreen
+
+object Routes {
+    const val ORDERS = "orders"
+    const val CLIENTS = "clients"
+}
+
+@Composable
+fun AppNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Routes.ORDERS) {
+        composable(Routes.ORDERS) {
+            OrderListScreen(navController)
+        }
+        composable(Routes.CLIENTS) {
+            ClientsListScreen(navController)
+        }
+    }
+    }
+}
