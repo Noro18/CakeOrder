79458a65ca9c755a37ac5b3d119d7a1cb878bb7e
Noro18 <zeroxg534@gmail.com>
2026-04-06 11:14:36 +0900

    chores(main): hamoos composable la presiza

--- app/src/main/java/com/example/ordermanagementcake/MainActivity.kt
+++ app/src/main/java/com/example/ordermanagementcake/MainActivity.kt
@@ -26,25 +26,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-
-    Button(
-        onClick ={}
-    ) {
-        Text(text = "Click Me")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrderManagementCakeTheme {
-        Greeting("Ediko")
-    }
-}
