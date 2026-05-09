70387f2624915003d8d5caa5a4037f7a832fd1b8
Noro18 <zeroxg534@gmail.com>
2026-05-09 10:07:45 +0900

    refactor(entitty): hadia naran entity cusotmer ba client

--- app/src/main/java/com/example/ordermanagementcake/data/local/entities/OrderEntity.kt
+++ app/src/main/java/com/example/ordermanagementcake/data/local/entities/OrderEntity.kt
@@ -14,14 +14,15 @@ import androidx.room.PrimaryKey
         childColumns = ["client_id"],
         onDelete = ForeignKey.CASCADE
     )],
-    indices = [Index("customer_id")]
+    indices = [Index("client_id")]
 )
 data class OrderEntity (
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "order_id") val id: Int = 0,
-    @ColumnInfo(name = "customer_id") val customerId: Int,
+    @ColumnInfo(name = "client_id") val customerId: Int,
     @ColumnInfo(name = "order_date") val orderDate: String,
     @ColumnInfo(name = "delivery_date") val deliveryDate: String,
     @ColumnInfo(name = "total_price") val totalPrice: Double,
-    @ColumnInfo(name = "order_notes") val orderNotes: String
+    @ColumnInfo(name = "order_notes") val orderNotes: String,
+    @ColumnInfo(name = "status") val status: OrderStatus = OrderStatus.PENDING // default value pending
 )
