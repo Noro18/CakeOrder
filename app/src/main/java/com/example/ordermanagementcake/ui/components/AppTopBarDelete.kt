ce7f485f39611dfecb09e9680b2a465d1fdc3bdb
Noro18 <zeroxg534@gmail.com>
2026-05-25 22:43:39 +0900

    new AppTopBarDelete.kt component for unique topbars

---
+++ app/src/main/java/com/example/ordermanagementcake/ui/components/AppTopBarDelete.kt
@@ -0,0 +1,64 @@
+package com.example.ordermanagementcake.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBarDelete(
+    title: String = "The Artisanal Bakery",
+    onMenuClick: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        },
+
+        navigationIcon = {
+            IconButton(onClick = { onMenuClick() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White
+                )
+            }
+        },
+
+        actions = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.White
+                )
+            }
+        },
+
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFFC23C12)
+        )
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TopBarDeletePreview() {
+    AppTopBarDelete()
+}
