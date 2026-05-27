b4a83f86021ccbe7ca3fdf4979cc79c473500987
Noro18 <zeroxg534@gmail.com>
2026-05-27 10:23:14 +0900

    udpate(Tier): refactor tier

--- app/src/main/java/com/example/ordermanagementcake/ui/forms/tier/TierFormScreen.kt
+++ app/src/main/java/com/example/ordermanagementcake/ui/forms/tier/TierFormScreen.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
