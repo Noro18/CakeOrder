764a0e47615ddc7b3de01665ce9a4337bea0a725
Noro18 <zeroxg534@gmail.com>
2026-06-16 10:43:55 +0900

    ORder ui State, added a ui state to store the full Order Detail

--- app/src/main/java/com/example/ordermanagementcake/ui/orders/OrderUiState.kt
+++ app/src/main/java/com/example/ordermanagementcake/ui/orders/OrderUiState.kt
@@ -1,10 +1,12 @@
 package com.example.ordermanagementcake.ui.orders
 
 import com.example.ordermanagementcake.data.local.entities.OrderStatus
+import com.example.ordermanagementcake.data.local.relations.OrderFullDetail
 import com.example.ordermanagementcake.data.local.relations.OrderWithCakes
 
 data class OrderUiState (
     val orders: List<OrderWithCakes> = emptyList(),
+    val selectedOrderDetail: OrderFullDetail? = null,
     val isLoading: Boolean = true,
     val errorMessage: String? = null,
     val selectedStatus: OrderStatus = OrderStatus.PENDING
